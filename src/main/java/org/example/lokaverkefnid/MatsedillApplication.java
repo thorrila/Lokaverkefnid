1d0c6a7e32c40dd05d3aad6f6863c2b3649f5c7d
thorrila <157368144+thorrila@users.noreply.github.com>
2024-04-09 14:20:50 +0000

    vidmotshlutir

--- src/main/java/org/example/lokaverkefnid/MatsedillApplication.java
+++ src/main/java/org/example/lokaverkefnid/MatsedillApplication.java
@@ -7,11 +7,12 @@ import javafx.stage.Stage;
 
 import java.io.IOException;
 
-public class HelloApplication extends Application {
+public class MatsedillApplication extends Application {
     @Override
     public void start(Stage stage) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("hello-view.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(), 320, 240);
+        FXMLLoader fxmlLoader = new FXMLLoader(MatsedillApplication.class.getResource("matsedill-view.fxml"));
+                    //Scene(Parent, width, height)
+        Scene scene = new Scene(fxmlLoader.load(), 600, 500);
         stage.setTitle("Hello!");
         stage.setScene(scene);
         stage.show();
---
+++ src/main/java/org/example/lokaverkefnid/MatsedillController.java
@@ -0,0 +1,36 @@
+package org.example.lokaverkefnid;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.ListView;
+import javafx.scene.control.TextField;
+
+/**
+ *
+ */
+public class MatsedillController {
+    //viðmótshlutir fyrir matseðil
+    @FXML
+    private TextField leitTextField;
+    @FXML
+    private ListView matsedillListView;
+    @FXML
+    private Button kvoldverdurButton;
+    @FXML
+    private Button morgunverdurButton;
+    @FXML
+    private Button hadegisverdurButton;
+    @FXML
+    private Button drykkirButton;
+    @FXML
+    private Button karfaButton;
+    //viðmótshlutir fyrir rétti
+
+    //viðmótshlutir fyrir körfu
+    @FXML
+    private Button karfaTilBaka;
+    @FXML
+    private Button karfaGreida;
+
+}
