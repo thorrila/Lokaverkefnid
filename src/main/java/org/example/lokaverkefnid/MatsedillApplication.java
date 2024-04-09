e7ed6acdc4cb389873123a02377bfa26d4c12efa
thorrila <157368144+thorrila@users.noreply.github.com>
2024-04-09 17:19:03 +0000

    update

--- src/main/java/org/example/lokaverkefnid/MatsedillApplication.java
+++ src/main/java/org/example/lokaverkefnid/MatsedillApplication.java
@@ -2,6 +2,7 @@ package org.example.lokaverkefnid;
 
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
 
@@ -10,12 +11,16 @@ import java.io.IOException;
 public class MatsedillApplication extends Application {
     @Override
     public void start(Stage stage) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(MatsedillApplication.class.getResource("matsedill-view.fxml"));
-                    //Scene(Parent, width, height)
-        Scene scene = new Scene(fxmlLoader.load(), 600, 500);
-        stage.setTitle("Hello!");
-        stage.setScene(scene);
+        //Loada fxml fyrir senu matseðils (forsíðu)
+        FXMLLoader matsedillLoader = new FXMLLoader(MatsedillApplication.class.getResource("matsedill-view.fxml"));
+        Parent matsedillRoot = matsedillLoader.load();
+        Scene matsedill = new Scene(matsedillRoot, 600, 500); //(parent, width, height)
+
+        //Setja upp senu fyrir matseðil
+        stage.setTitle("Lokaverkefni");
+        stage.setScene(matsedill);
         stage.show();
+
     }
 
     public static void main(String[] args) {
